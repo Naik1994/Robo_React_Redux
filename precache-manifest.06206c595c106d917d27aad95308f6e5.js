1d0ee397e4df64fcf95874b7393a0c6ee67286cf
Harish Naik <harishnaik.bs@aceturtle.com>
2019-10-08 16:14:13 +0530

    Updates

---
+++ precache-manifest.06206c595c106d917d27aad95308f6e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd2967bcc59cb874d924310db73b8d0",
+    "url": "/Robo_React_Redux/index.html"
+  },
+  {
+    "revision": "8b26fca4efded57da1bb",
+    "url": "/Robo_React_Redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "536f099c931b86a839de",
+    "url": "/Robo_React_Redux/static/css/main.ac87e0f5.chunk.css"
+  },
+  {
+    "revision": "8b26fca4efded57da1bb",
+    "url": "/Robo_React_Redux/static/js/2.39ec0b78.chunk.js"
+  },
+  {
+    "revision": "536f099c931b86a839de",
+    "url": "/Robo_React_Redux/static/js/main.f77b0c01.chunk.js"
+  },
+  {
+    "revision": "fd29a95970257087eb06",
+    "url": "/Robo_React_Redux/static/js/runtime~main.9a8c5d70.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Robo_React_Redux/static/media/SEGA.82e09f78.TTF"
+  }
+]);
